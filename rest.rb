77a4433445a4cb99562b766a7b577b717f807745
kopzu <nk.kopio@gmail.com>
2013-10-02 21:44:43 +0300

    Add more mockdata Change Title

--- rest.rb
+++ rest.rb
@@ -2,10 +2,12 @@ require 'sinatra'
 require 'json'
 
 get '/players' do
-  players = 
-    [
-      {:id => 1, :nick => "vile"}, 
-      {:id => 2, :nick => "kopi"}
-    ]
-  players.to_json
+[
+  {id: 1, nick: "vile"},
+  {id: 2, nick: "kopi"},
+  {id: 3, nick: "gooby"},
+  {id: 4, nick: "dolan"},
+  {id: 5, nick: "scrooge"},
+  {id: 6, nick: "Q"}
+].to_json
 end
