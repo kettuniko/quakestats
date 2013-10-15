d1a251066e8143f132e06d345c23767e745b6446
vile <vile@lovebo.at>
2013-10-16 00:47:19 +0300

    Added a basic parser

--- rest.rb
+++ rest.rb
@@ -1,13 +1,53 @@
 require 'sinatra'
 require 'json'
+require 'xml'
+require 'xmlsimple'
+
+require 'pp'
+
+class QuakeStatParser 
+  attr_accessor :players, :cache_time, :cache_reset_timer
+
+  def initialize()
+    @players = Hash.new
+    @cache_time = Time.now
+    @cache_reset_timer = 120.0 # 120 seconds for now
+  end
+
+  def calculate_stats()
+    parse_match('logs/ffa_3[lacrima]050612-1354.xml')
+    parse_match('logs/ffa_2[ukpak5]240413-1355.xml')
+    @cache_time = Time.now + @cache_reset_timer 
+  end
+
+  def parse_match(xml)
+    puts "Parsing match #{xml}"
+    log = XmlSimple.xml_in(xml, {'ForceArray' => false})
+
+    puts log['match_info']['timestamp']
+
+    log['events']['event'].each do |event|
+      if (event['death']) 
+        death = event['death']
+        attacker = death['attacker']
+        if (!players[attacker])
+          puts "> a new player found #{attacker}"
+          @players[attacker] = Hash.new
+          @players[attacker]['frags'] = 0
+        end
+        @players[attacker]['frags'] += 1
+      end
+    end
+  end
+end
+
+parser = QuakeStatParser.new
 
 get '/players' do
-[
-  {id: 1, nick: "vile"},
-  {id: 2, nick: "kopi"},
-  {id: 3, nick: "gooby"},
-  {id: 4, nick: "dolan"},
-  {id: 5, nick: "scrooge"},
-  {id: 6, nick: "Q"}
-].to_json
+  if (parser.cache_time < Time.now)
+    puts "cache reseted, calculating new stats"
+    parser.calculate_stats()
+  end
+
+  pp parser.players.to_json
 end
