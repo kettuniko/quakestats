96a8c44f505ab31ca0cae37e74bb76a1f29850d1
Niko <nk.kopio@gmail.com>
2013-10-27 20:57:58 +0200

    Modify rest.rb to return an array instead of object

--- ws/rest.rb
+++ ws/rest.rb
@@ -7,20 +7,36 @@ require 'pp'
 
 before do
   content_type :json
+  headers 'Access-Control-Allow-Origin' => '*',
+          'Access-Control-Allow-Methods' => %w(GET)
 end
 
 class QuakeStatParser
   attr_accessor :players, :cache_time, :cache_reset_timer
 
   def initialize
-    @players = Hash.new
+    @frags = 'frags'
+    @name = 'name'
+
+    @players = Array.new
+    @players_hash = Hash.new
     @cache_time = Time.now
     @cache_reset_timer = 120.0 # 120 seconds for now
   end
 
   def calculate_stats
+    @players_hash = Hash.new
     parse_match('logs/ffa_3[lacrima]050612-1354.xml')
     parse_match('logs/ffa_2[ukpak5]240413-1355.xml')
+
+    @players = Array.new
+    @players_hash.each do |key, value|
+      player = Hash.new
+      player[@name] = key
+      player[@frags] = value[@frags]
+      @players.push(player)
+    end
+
     @cache_time = Time.now + @cache_reset_timer
   end
 
@@ -34,14 +50,15 @@ class QuakeStatParser
       if event['death']
         death = event['death']
         attacker = death['attacker']
-        unless players[attacker]
+        unless @players_hash[attacker]
           puts "> a new player found #{attacker}"
-          @players[attacker] = Hash.new
-          @players[attacker]['frags'] = 0
+          @players_hash[attacker] = Hash.new
+          @players_hash[attacker][@frags] = 0
         end
-        @players[attacker]['frags'] += 1
+        @players_hash[attacker][@frags] += 1
       end
     end
+
   end
 end
 
@@ -52,8 +69,5 @@ get '/players' do
     puts 'cache reset, calculating new stats'
     parser.calculate_stats()
   end
-  response['Access-Control-Allow-Methods']= ['GET']
-  #TODO harden
-  response['Access-Control-Allow-Origin'] = '*'
   pp parser.players.to_json
 end
